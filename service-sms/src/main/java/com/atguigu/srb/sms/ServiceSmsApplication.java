5e91b911d5d70c49ea3e0d982ca1765b25061ffd
pp <2480949450@qq.com>
2022-11-02 21:09:03 +0800

    启动类

---
+++ service-sms/src/main/java/com/atguigu/srb/sms/ServiceSmsApplication.java
@@ -0,0 +1,15 @@
+package com.atguigu.srb.sms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+@ComponentScan({"com.atguigu.srb.common","com.atguigu.srb.sms","com.atguigu.srb.base"})
+public class ServiceSmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceSmsApplication.class,args);
+    }
+}
