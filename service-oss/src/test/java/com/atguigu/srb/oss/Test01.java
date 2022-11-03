9cbc05a1ae88114a5875edcabaa7f16ce0d12795
tototome <71238316+tototome@users.noreply.github.com>
2022-11-03 17:07:32 +0800

    Update Test01.java

--- service-oss/src/test/java/com/atguigu/srb/oss/Test01.java
+++ service-oss/src/test/java/com/atguigu/srb/oss/Test01.java
@@ -26,8 +26,8 @@ public class Test01 {
         // Endpoint以华东1（杭州）为例，其它Region请按实际情况填写。
         String endpoint = "https://oss-cn-shenzhen.aliyuncs.com";
         // 阿里云账号AccessKey拥有所有API的访问权限，风险很高。强烈建议您创建并使用RAM用户进行API访问或日常运维，请登录RAM控制台创建RAM用户。
-        String accessKeyId = "LTAI5tCkg8thzV2vsxt5NSfg";
-        String accessKeySecret = "wWrPV2Pxyax8rC3FFuZlWL8b3wzKmc";
+        String accessKeyId = "";
+        String accessKeySecret = "";
         // 填写Bucket名称，例如examplebucket。
         String bucketName = "srb0608";
         // 填写Object完整路径，完整路径中不能包含Bucket名称，例如exampledir/exampleobject.txt。
